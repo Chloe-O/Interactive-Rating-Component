ff7e14c461934041d77061d83bd4f30139da57f4
Chloe-O <chloeosbourne@yahoo.co.uk>
2023-09-28 22:20:31 +0100

    added modal

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const ratingMessage = document.getElementById("submitted-rating");
 const submit = document.getElementById("submit");
 const ratingCard = document.getElementById("rating-card");
 const submittedCard = document.getElementById("submitted-card");
+const modal = document.getElementById("modal-overlay");
+const modalClose = document.getElementById("close");
 let selectedRating;
 
 numberChoices.forEach((i) => {
@@ -20,9 +22,14 @@ numberChoices.forEach((i) => {
 submit.addEventListener("click", () => {
   if (!selectedRating) {
     console.log("pick a number!");
+    modal.style.display = "block";
   } else {
     ratingMessage.innerText = `You selected ${selectedRating} out of 5`;
     ratingCard.style.display = "none";
     submittedCard.style.display = "flex";
   }
+});
+
+modalClose.addEventListener("click", () => {
+  modal.style.display = "none";
 });
