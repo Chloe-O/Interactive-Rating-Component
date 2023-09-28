573ee6681e68be68169f66510572bacc7b038f2f
Chloe-O <chloeosbourne@yahoo.co.uk>
2023-09-28 21:00:38 +0100

    rating collected on submit

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-let numberContainer = document.getElementById("numberContainer");
-let numberChoices = document.querySelectorAll(".number-rating");
+const numberChoices = document.querySelectorAll(".number-rating");
+const ratingMessage = document.getElementById("submitted-rating");
+const submit = document.getElementById("submit");
+const ratingCard = document.getElementById("rating-card");
+const submittedCard = document.getElementById("submitted-card");
 let selectedRating;
 
-function collectRating() {
-  numberChoices.forEach((i) => {
-    i.addEventListener("click", () => {
-      numberChoices.forEach((e) => {
-        if (e.classList.contains("selected")) {
-          e.classList.remove("selected");
-        }
-      });
-      i.classList.add("selected");
-      selectedRating = parseInt(i.innerHTML);
+numberChoices.forEach((i) => {
+  i.addEventListener("click", () => {
+    numberChoices.forEach((e) => {
+      if (e.classList.contains("selected")) {
+        e.classList.remove("selected");
+      }
     });
+    i.classList.add("selected");
+    selectedRating = parseInt(i.innerHTML);
   });
+});
 
-  console.log(selectedRating);
-}
-
-collectRating();
+submit.addEventListener("click", () => {
+  if (!selectedRating) {
+    console.log("pick a number!");
+  } else {
+    ratingMessage.innerText = `You selected ${selectedRating} out of 5`;
+    ratingCard.style.display = "none";
+    submittedCard.style.display = "flex";
+  }
+});
