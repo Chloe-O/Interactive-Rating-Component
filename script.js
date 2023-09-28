5d2021d40c6c1b77a752c04f5bc5f836c89aefb0
Chloe-O <chloeosbourne@yahoo.co.uk>
2023-09-28 20:37:45 +0100

    submitted rating screen

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+let numberContainer = document.getElementById("numberContainer");
+let numberChoices = document.querySelectorAll(".number-rating");
+let selectedRating;
 
-$(".numbers span").each(() => {
-  console.log( $('this span') )
-});
+function collectRating() {
+  numberChoices.forEach((i) => {
+    i.addEventListener("click", () => {
+      numberChoices.forEach((e) => {
+        if (e.classList.contains("selected")) {
+          e.classList.remove("selected");
+        }
+      });
+      i.classList.add("selected");
+      selectedRating = parseInt(i.innerHTML);
+    });
+  });
+
+  console.log(selectedRating);
+}
+
+collectRating();
